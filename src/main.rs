bd6b878e5c8f22211a6194f1f4e36033f98a7f5e
Mike <mikesneider@gmail.com>
2022-01-14 15:53:10 -0500

    guessing number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use std::io;
+
+/*Si recuerdo C++ la librería std io es para input - output, wow! */ 
 fn main() {
-    println!(" --- ");
-    println!("Hello, world!  From CARGO");
+    println!("Adivina el numerito!");
+
+    let mut guess = String::new();
+    let mut algo = "Mike";
+    /*con let declaro variable, (Como BASIC) :: es para el tipo de dato */
+    /* Qué es "MUT?"*/ 
+    /*"mut" es para variables mutables, "sin mut" para inmutables, "mu" para vacas 😁 */ 
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+    //io::stdin().read_line(&mut algo);
+    println!("tu algo {}",algo);
 }
